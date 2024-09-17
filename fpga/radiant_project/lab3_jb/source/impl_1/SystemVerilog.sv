6707dab204331dd166d3aec64eaa6211c7cbd2b3
Jason Bowman <jbowman@g.hmc.edu>
2024-09-17 10:06:27 -0700

    Multiple driver issues

--- fpga/radiant_project/lab3_jb/source/impl_1/SystemVerilog.sv
+++ fpga/radiant_project/lab3_jb/source/impl_1/SystemVerilog.sv
@@ -6,10 +6,10 @@
 module top (
 	input logic reset,
 	input logic [2:0] col,
-	input logic [3:0] button,
-	input logic int_osc,
 	input logic [24:0] counter,
 	input logic button_press,
+	input logic[6:0] newseg,
+	input logic[6:0] prevseg,
 	output logic [6:0] seg,
 	output logic [1:0] osc,
 	output logic [3:0] r_sel
@@ -30,13 +30,10 @@ end
 //////////////////////////////////////////////////////////////////
 
 
-
-
-
 // instantiated submodules
 button_decoder MOD1 (reset, col, r_sel, button);
 
-seven_seg_decoder MOD2 (reset, button, int_osc, counter, osc, button_press, seg);
+seven_seg_decoder MOD2 (reset, button, int_osc, counter, button_press, osc, seg, newseg, prevseg);
 
 count_osc_sel_button MOD3 (reset, col, int_osc, counter, osc, button_press, r_sel);
 
@@ -51,6 +48,7 @@ module button_decoder (
 	output logic [3:0] button
 	);
 	
+	assign button = 4'b0001;
 	
 endmodule
 //////////////////////////////////////////////////
@@ -61,31 +59,17 @@ module seven_seg_decoder (
 	input logic [3:0] button,
 	input logic int_osc,
 	input logic [24:0] counter,
-	input logic [1:0] osc,
 	input logic button_press,
-	output logic [6:0] seg
+	output logic [1:0] osc,
+	output logic [6:0] seg,
+	output logic[6:0] newseg,
+	output logic[6:0] prevseg
 	);
 	
 	
 // Instantiate internal segment variables
-logic [6:0] newseg;
-logic [6:0] prevseg;
 //////////////////////////////////////////////////////////////////
 
-
-// controls display toggle speed and updates corresponding inputs for binary-to-hexadecimal combinational logic
-always_ff @(posedge int_osc) begin
-    // counter[#] controlls toggle speed
-	if (counter[12] == 0) begin
-		seg <= newseg;
-		osc <= 2'b01;
-	end
-    else begin
-		seg <= prevseg;
-		osc <= 2'b10;
-    end
-end
-//////////////////////////////////////////////////////////////////
 
 // update newseg////////////////////////////////////////////
 	always_comb begin
@@ -128,14 +112,16 @@ endmodule
 //===============================================
 //////////////////////////////////////////////////
 module count_osc_sel_button(
-	input logic reset
+	input logic reset,
 	input logic [2:0] col,
 	output logic int_osc,
-	output logic [24:0] counter,
+	output logic [24:0] int_counter,
 	output logic [1:0] osc,
 	output logic button_press,
 	output logic [3:0] r_sel
 	);
+	
+	
 	
 // HSOSC instantiation ////////////////////////////////////
 HSOSC #(.CLKHF_DIV(2'b01)) 
@@ -145,20 +131,22 @@ HSOSC #(.CLKHF_DIV(2'b01))
 // Counter block //////////////////////////////////////////
 always_ff @(posedge int_osc) begin
     if (!reset)
-		counter <= 24'b0;
+		int_counter <= 24'b0;
     else
-        counter <= counter + 1'b1;
+        int_counter <= int_counter + 1'b1;
 end
+
+assign counter = int_counter;
 ///////////////////////////////////////////////////////////
 
 // row select (toggle through rows)////////////////////////
 always_comb begin
-	case(counter[6:5])
+	case(int_counter[6:5])
 		2'b00: r_sel = 4'b0001;
 		2'b01: r_sel = 4'b0010;
 		2'b10: r_sel = 4'b0100;
 		2'b11: r_sel = 4'b1000;
-		defaultL r_sel = 4'bxxxx;
+		default r_sel = 4'bxxxx;
 	endcase
 end
 ///////////////////////////////////////////////////////////
@@ -166,7 +154,7 @@ end
 // oscillator block ///////////////////////////////////////
 always_ff @(posedge int_osc) begin
     // counter[#] controlls toggle speed
-	if (!counter[12]) begin
+	if (!int_counter[12]) begin
 		osc <= 2'b01;
 	end
     else begin
