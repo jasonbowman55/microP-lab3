78013c0341fa72be68e27b74a5bb442fb2fbb54a
jasonbowman55 <95401132+jasonbowman55@users.noreply.github.com>
2024-09-28 18:26:00 -0700

    working r_sel

--- fpga/radiant_project/lab3_jb/source/impl_1/SystemVerilog.sv
+++ fpga/radiant_project/lab3_jb/source/impl_1/SystemVerilog.sv
@@ -5,31 +5,15 @@
 
 module top (
 	input logic reset,
-	input logic clk,
-	input logic [3:0] col,
 	output logic [3:0] r_sel,
-	output logic [6:0] seg,
-	output logic [1:0] osc,
-	output logic debug_clk
-
+	output logic clk
 	);
-
-logic [24:0]counter;
-logic [3:0] col_sync;
-logic [3:0] select;
-logic  debounce;
-logic [3:0] right;
-logic [3:0] left;
-//logic clk;
-logic [24:0] clk_slow;
-
-assign debug_clk = clk;
-
-//logic clk; TAKEN OUT FOR MODEL SIM/
-
-// HSOSC instantiation //////////////////////////////////// TAKEN OUT FOR MODEL SIM
-//HSOSC #(.CLKHF_DIV(2'b01)) 
-//	hf_osc (.CLKHFPU(1'b1), .CLKHFEN(1'b1), .CLKHF(int_osc));
+	
+	logic [24:0] clk_slow;
+	
+	// HSOSC instantiation //////////////////////////////////// TAKEN OUT FOR MODEL SIM
+HSOSC #(.CLKHF_DIV(2'b01)) 
+	hf_osc (.CLKHFPU(1'b1), .CLKHFEN(1'b1), .CLKHF(int_osc));
 ///////////////////////////////////////////////////////////
 	always_ff @(posedge int_osc) begin
 		if (!reset) begin
@@ -40,30 +24,102 @@ assign debug_clk = clk;
 	end
 
 assign clk = clk_slow[15];
+	
+// defining state variables /////
+	logic [19:0] state, nextstate;
+	logic [3:0] button;
+	
+	parameter S0 = 12'b000000000001;
+	parameter S1 = 12'b000000000010;
+	parameter S2 = 12'b000000000100;
+	parameter S3 = 12'b000000001000;
+	parameter S4 = 12'b000000010000;
+	parameter S5 = 12'b000000100000;
+	parameter S6 = 12'b000001000000;
+	parameter S7 = 12'b000010000000;
+	parameter S8 = 12'b000100000000;
+	parameter S9 = 12'b001000000000;
+	parameter S10 = 12'b010000000000;
+	parameter S11 = 12'b100000000000;
+///////////////////////////////////
 
-always_ff @(posedge clk) begin
-    // counter[#] controlls toggle speed
-	if (counter[4] == 0) begin
-		select <= right; // Select left DIP-switch input
-		osc[0] <= 1; // turn on left display
-		osc[1] <= 0;
+// next state = nextstate on every clk edge
+	always_ff @(posedge clk)
+		if (!reset) 
+			state <= S0;
+		else 	
+			state <= nextstate;	
+	
+always_comb
+    case(state)
+        S0: if (r_sel == 4'b1111) begin
+				nextstate = S4;
+			end else begin
+				nextstate = S1;
+			end
+		S4: nextstate = S8;
+		S8: if (r_sel == 4'b1111) begin
+				nextstate = S8;
+			end else begin
+				nextstate = S0;
+			end
+		
+		
+		
+		
+		S1: if (r_sel == 4'b1111) begin
+				nextstate = S5;
+			end else begin
+				nextstate = S2;
+			end
+		S5: nextstate = S9;
+		S9: if (r_sel == 4'b1111) begin
+				nextstate = S9;
+			end else begin
+				nextstate = S1;
+			end
+			
+			
+			
+			
+		S2: if (r_sel == 4'b1111) begin
+				nextstate = S6;
+			end else begin
+				nextstate = S3;
+			end
+		S6: nextstate = S10;
+		S10: if (r_sel == 4'b1111) begin
+				nextstate = S10;
+			end else begin
+				nextstate = S2;
+			end
+			
+			
+			
+			
+		S3: if (r_sel == 4'b1111) begin
+				nextstate = S7;
+			end else begin
+				nextstate = S0;
+			end
+		S7: nextstate = S11;
+		S11: if (r_sel == 4'b1111) begin
+				nextstate = S11;
+			end else begin
+				nextstate = S3;
+			end
+
+        default: nextstate = S0;   
+    endcase
+
+// row scan based on state /////////////////////////////////////////////
+	always_comb begin
+		r_sel = (state == S0 || state == S4 || state == S8) ? 4'b1110 :
+				(state == S1 || state == S5 || state == S9) ? 4'b1101 :
+				(state == S2 || state == S6 || state == S10) ? 4'b1011 :
+				(state == S3 || state == S7 || state == S11) ? 4'b0111 :
+				4'b1111; // Default value
 	end
-    else if (counter[4] == 1) begin //CHANGE
-        	select <= left; // Select right DIP-switch input
-		osc[0] <= 0;
-		osc[1] <= 1; // turn on right display
-    end
-end
-
-
-// instantiated submodules
-button_decoder_bounce MOD1 (reset, col_sync, clk, counter, right, left, r_sel);
-
-seven_seg_decoder MOD2 (select, seg);
-
-syncronizer MOD3 (reset, col, clk, col_sync, counter);
-
-debouncer MOD4 (reset, clk, col_sync, debounce);
 ///////////////////////////
 
 
